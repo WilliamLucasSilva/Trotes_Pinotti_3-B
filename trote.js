cac7fa7486ac6da84dc9c62bf6a4afea74367cdc
William Lucas <106882851+WilliamLucasSilva@users.noreply.github.com>
2024-03-15 11:01:23 -0300

    Update trote.js

--- trote.js
+++ trote.js
@@ -1,5 +1,15 @@
-let trotes = [
+class trote{
+    constructor(title, date, author, img, paragraph){
+        this.title = title
+        this.date = date
+        this.author = author
+        this.img = new image(100, 100)
+        this.img.src = img
+        this.paragraph = paragraph
+    }
+}
 
+export const trotes = [
+    new trote("dia da mulher", "08/03/2024", "William Lucas da Silva Ribeiro", "", "Troterealiao m homenagem ao dia das molieres")
 ]
 
-export default trotes
